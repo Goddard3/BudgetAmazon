8b506613edb8cb72b86e007a184c9083acf25046
Jonathan C Bevis <jbevis1@student.wiregrass.edu>
2024-03-28 08:40:51 -0400

    added Login and Sign up

--- NotAmazon/urls.py
+++ NotAmazon/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 # directory list
 urlpatterns = [
-    path("home", views.index, name="index"),
+    path("home", views.index, name="home"),
     path("products", views.products, name="products"),
     path("shop", views.shop, name="shop"),
-    path("logIn", views.logIn, name="logIn"),
-    path("signUp", views.signUp, name="signUp")
+    path("user_login", views.user_login, name="user_login"),
+    path("user_register", views.user_register, name="user_register")
 ]
--- NotAmazon/views.py
+++ NotAmazon/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
 from .models import Item
+
+# Creating a Register form class
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model=User
+        fields = ["username", "email", "password1", "password2"]
 
 # Create your views here.
 def index(request):
@@ -18,8 +28,34 @@ def shop(request):
     context = {"item_list": item_list}
     return render(request, "shop.html", context)
 
-def logIn(request):
-    return render(request, "logIn.html", {})
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        username = request.POST["username"]
+        password2 = request.POST["password"]
 
-def signUp(request):
-    return render(request, "signUp.html", {})
+        user = authenticate(request, username=username, password=password2)
+
+        if user is not None:
+            form = login(request, user)
+
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "user_login.html", {"form":form})
+
+def user_register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegistrationForm()
+
+    return render(request, "user_register.html", {"form":form})
+
+#def signUp(request):
+#    return render(request, "signUp.html", {})
